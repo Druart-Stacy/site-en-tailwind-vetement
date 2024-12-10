62ab0c5b8afe448b4aa248ce0aefc14f97eb1289
Stacy Druart <stacy@StacyDruartMSI>
2024-12-10 13:03:20 +0100

    maj

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], // Scanne tous les fichiers HTML et JS dans src
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
